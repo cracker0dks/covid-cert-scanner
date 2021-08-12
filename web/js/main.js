64738e0276134e85058f62a3aa517ce3d23c64a3
raphael <raphael@cloud13.de>
2021-08-13 00:09:38 +0200

    fix subdir

--- web/js/main.js
+++ web/js/main.js
@@ -1,3 +1,10 @@
+var url = document.URL.substr(0, document.URL.lastIndexOf('/'));
+var urlSplit = url.split("/");
+var subdir = "";
+for (var i = 3; i < urlSplit.length; i++) {
+    subdir = subdir + '/' + urlSplit[i];
+}
+
 $(document).ready(function () {
     $("#openreader-btn").qrCodeReader({
         target: "#target-input",
@@ -6,7 +13,7 @@ $(document).ready(function () {
         skipDuplicates: true,
         callback: function (codes) {
             $("#log").text(JSON.stringify(codes));
-            $.get("/validate", { data: codes }, function (res) {
+            $.get(subdir+"/validate", { data: codes }, function (res) {
                 console.log(res)
                 $("#log2").text(res)
             });
